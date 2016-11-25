780b38d7796f272b0f364ef76ccabe6cb56acbb2
Ankush Malik <ankushmalik631@gmail.com>
2016-11-25 20:10:48 +0530

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :comments
+  devise_for :users
+  resources :links do
+  	member do
+  	  put "like",   to: "links#upvote"
+  	  put "dislike",to: "links#downvote"
+  	end
+  	resources :comments
+  end
+  #delete 'comments/:id' => 'comments#destroy' , as: 'comment_destroy'
+  root to: 'links#index'
+end
